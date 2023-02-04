d6c2523076f0519a188eeb1b0975eecad39978ee
Muhammad Asmaul Lutfi <asmaullutfi09@gmail.com>
2023-02-04 14:39:25 +0700

    pertama

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
 @section('container')
 <div class="grid grid-cols-3 mx-auto">
 
-@foreach ($coba as $data)    
+    @foreach ($coba as $data)
 
     <div class="shadow-md p-2 h-96 overflow-hidden">
         <img src="{{ asset('storage/'.$data->image) }}" alt="" class="w-[300px] object-cover">
@@ -12,8 +12,9 @@
         <p class="mt-4">{{ $data->deskripsi }}</p>
     </div>
 
-@endforeach
+    @endforeach
 
 </div>
+<p>coba</p>
 
 @endsection
