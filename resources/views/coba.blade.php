f98b1682bd2e3637daf51983bc96b13ca93fd897
ryan <yuliantoryan64@gmail.com>
2023-02-04 14:07:01 +0700

    first update

--- resources/views/coba.blade.php
+++ resources/views/coba.blade.php
@@ -5,5 +5,5 @@
 <h1 class="text-red-600">test</h1>
 <h1 class="text-red-600">test</h1>
 <h1 class="text-red-600">test</h1>
-
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem sunt explicabo nemo suscipit distinctio voluptas eius illo commodi! Ut officia eos quibusdam maiores! Praesentium nemo eligendi veritatis, quasi maxime sapiente!</p>
 @endsection
