fe096265dd9ef83ee537660020445505ffb6b505
Andhi <agungandhita@gmail.com>
2023-02-04 20:01:49 +0700

    first

--- resources/views/partials/dropdown.blade.php
+++ resources/views/partials/dropdown.blade.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/css/app.css')
-    <title>Ini dari head</title>
+    <title>Document</title>
 </head>
-
-<body> 
+<body>
     
 </body>
+</html>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,10 +1,27 @@
-<header class="bg-blue-600 w-full p-2">
-    <div class="container">
-        <h1>judul</h1>
-        <ul class="flex justify-between">
-            <li>home</li>
-            <li>home</li>
-            <li>home</li>
-        </ul>
-    </div>
-</header>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div className='fixed z-40 w-full py-4 backdrop-blur-sm bg-black/10'>
+        <div className="container">
+          <div className='flex justify-between'>
+          <img src='/img/logo.png' className='w-22 h-14 my-auto '/>
+              <ul className='flex gap-x-8 my-auto'>
+                <li className='list-none text-white text-lg font-bold cursor-pointer'><a href="/">Home</a> </li>
+                <li className='list-none text-white text-lg font-bold cursor-pointer'></li>
+                <li className='list-none text-white text-lg font-bold cursor-pointer'><a href='/destination'>Booking</a></li>
+              </ul>
+              <div className="flex gap-x-2">
+                <a href="masuk" className='bg-dark p-2 rounded-md text-white inline-block my-auto'>Login</a>
+                <a href="daftar" className='bg-dark p-2 rounded-md text-white inline-block my-auto'>Register</a>
+                </div>   
+          </div>
+          </div>
+      </div>
+</body>
+</html>
