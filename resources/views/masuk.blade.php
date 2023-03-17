fbf7df8d4c9e21196c1e9e20e7f894e3f2224be6
Andhi <agungandhita@gmail.com>
2023-03-17 14:24:55 +0700

    baru

--- resources/views/masuk.blade.php
+++ resources/views/masuk.blade.php
@@ -73,7 +73,7 @@
 
     
       <div class="flex justify-center my-7">
-        <button type="submit"  class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150">Masuk</button>
+        <button type="submit"  class="inline-block px-6 py-2.5 bg-red-600 text-white font-bold text-lg leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150">Masuk</button>
       </div>
 
 
--- resources/views/type.blade.php
+++ resources/views/type.blade.php
@@ -11,7 +11,7 @@
           <circle cx="11" cy="11" r="8" class=""></circle>
           <line x1="21" y1="21" x2="16.65" y2="16.65" class=""></line>
         </svg>
-        <input type="name" name="search" class="h-14 w-full rounded-md py-4 pr-40 pl-12 outline-none focus:ring-2" placeholder="City, Address, Zip :" value="{{ request('search') }}"/>
+        <input type="name" name="search" class="h-14 w-full rounded-md py-4 pr-40 pl-12 outline-none focus:ring-2" placeholder="City, Destination" value="{{ request('search') }}"/>
         <button type="submit" class="absolute right-0 mr-1 inline-flex h-12 items-center justify-center rounded-lg bg-gray-900 px-10 font-medium text-white focus:ring-4 hover:bg-gray-700">Search</button>
       </form>
     </div>
