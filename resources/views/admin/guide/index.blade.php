abe8168c381bf28fe667f5a2fe93facc31128508
Andhi <agungandhita@gmail.com>
2023-05-08 04:47:10 +0700

    baru

--- resources/views/admin/guide/index.blade.php
+++ resources/views/admin/guide/index.blade.php
@@ -105,12 +105,12 @@
 id="exampleModalLong-{{ $edit->id }}" tabindex="-1" aria-labelledby="exampleModalLongLabel" aria-hidden="true">
 <div class="modal-dialog relative w-auto pointer-events-none">
   <div
-    class="modal-content border-none shadow-lg relative flex flex-col w-[800px] pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current m-auto">
+    class="modal-content border-none shadow-lg relative flex flex-col w-[800px] pointer-events-auto dark:bg-slate-800 bg-white bg-clip-padding rounded-md outline-none text-current m-auto">
     <div
       class="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
          
          <form action="/admin/guide/edit/{{ $edit->id }}" method="POST" class="w-full px-4" enctype="multipart/form-data">
-            <h1 class="text-center font-semibold text-2xl">EDIT GUIDE</h1>
+            <h1 class="text-center font-semibold text-2xl dark:text-white">EDIT GUIDE</h1>
             @csrf
 
             <img src="{{ asset('image/'.$user->image) }}" alt="" class="flex w-36 h-36 justify-center object-cover">
@@ -120,16 +120,16 @@ id="exampleModalLong-{{ $edit->id }}" tabindex="-1" aria-labelledby="exampleModa
             </div>
 
             <div class="mt-4">
-                <label for="nama">Name</label>
-                <input type="text" name="nama" id="nama" class="w-full h-12 rounded-md p-2 border mt-4" value="{{ $edit->nama }}">
+                <label class="dark:text-white font-semibold" for="nama">Name</label>
+                <input type="text" name="nama" id="nama" class="dark:bg-gray-600 dark:text-white w-full h-12 rounded-md p-2 border mt-4" value="{{ $edit->nama }}">
             </div>
             <div class="mt-4">
-              <label for="no_tlpn">Telephone</label>
-              <input type="number" name="no_tlpn" id="no_tlpn" class="w-full h-12 rounded-md p-2 border mt-4" value="{{ $edit->no_tlpn }}">
+              <label class="dark:text-white font-semibold" for="no_tlpn">Telephone</label>
+              <input type="number" name="no_tlpn" id="no_tlpn" class="dark:bg-gray-600 dark:text-white w-full h-12 rounded-md p-2 border mt-4" value="{{ $edit->no_tlpn }}">
           </div>
           <div class="mt-4">
-            <label for="alamat">Address</label>
-            <input type="text" name="alamat" id="alamat" class="w-full h-12 rounded-md p-2 border mt-4" value="{{ $edit->alamat }}">
+            <label class="dark:text-white font-semibold" for="alamat">Address</label>
+            <input type="text" name="alamat" id="alamat" class="dark:bg-gray-600 dark:text-white w-full h-12 rounded-md p-2 border mt-4" value="{{ $edit->alamat }}">
         </div>
     
             <div class="flex gap-x-4 mt-4">
@@ -169,15 +169,15 @@ id="exampleModalLong" tabindex="-1" aria-labelledby="exampleModalLongLabel" aria
 
             <div class="mt-4">
                 <label class="dark:text-white font-semibold" for="nama">Name</label>
-                <input type="text" name="nama" id="nama" class="w-full h-12 rounded-md p-2 border mt-4" >
+                <input type="text" name="nama" id="nama" class="dark:bg-gray-600 dark:text-white w-full h-12 rounded-md p-2 border mt-4" >
             </div>
             <div class="mt-4">
               <label class="dark:text-white font-semibold" for="no_tlpn">Telephon</label>
-              <input type="number" name="no_tlpn" id="no_tlpn" class="w-full h-12 rounded-md p-2 border mt-4" >
+              <input type="number" name="no_tlpn" id="no_tlpn" class="dark:bg-gray-600 dark:text-white w-full h-12 rounded-md p-2 border mt-4" >
           </div>
           <div class="mt-4">
             <label class="dark:text-white font-semibold" for="alamat">Address</label>
-            <input type="text" name="alamat" id="alamat" class="w-full h-12 rounded-md p-2 border mt-4" >
+            <input type="text" name="alamat" id="alamat" class="dark:bg-gray-600 dark:text-white w-full h-12 rounded-md p-2 border mt-4" >
           </div>
             <div class="flex gap-x-4 mt-4">
             <button type="submit" class="bg-green-600 py-2 px-4 rounded-md text-white">Send</button>
