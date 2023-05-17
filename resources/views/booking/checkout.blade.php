32f3365538336175ad0e199cdebac010294757f2
Andhi <agungandhita@gmail.com>
2023-05-17 21:46:53 +0700

    baru

--- resources/views/booking/checkout.blade.php
+++ resources/views/booking/checkout.blade.php
@@ -55,43 +55,17 @@
                     </div>
 
                     <div class="flex justify-between mt-4">
-                      <div class="">
-                          <h1 for="first_name" class="block text-lg font-medium text-gray-900 dark:text-white">Teen Group
-                          </h1>
-                          <p class="text-sm text-red-600">13-17 years old</p>
-                      </div>
+                        <div class="">
+                            <h1 for="first_name" class="block text-lg font-medium text-gray-900 dark:text-white">child Group
+                            </h1>
+                            <p class="text-sm text-red-600">5-12 years old</p>
+                        </div>
 
-                      <div class="">
-                          <input type="number" id="first_name"
-                              class="my-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-32 h-8 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                      </div>
-                  </div>
-
-                  <div class="flex justify-between mt-4">
-                    <div class="">
-                        <h1 for="first_name" class="block text-lg font-medium text-gray-900 dark:text-white">child Group
-                        </h1>
-                        <p class="text-sm text-red-600">5-12 years old</p>
+                        <div class="">
+                            <input type="number" id="first_name"
+                                class="my-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-32 h-8 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        </div>
                     </div>
-
-                    <div class="">
-                        <input type="number" id="first_name"
-                            class="my-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-32 h-8 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                    </div>
-                </div>
-
-                <div class="flex justify-between mt-4">
-                  <div class="">
-                      <h1 for="first_name" class="block text-lg font-medium text-gray-900 dark:text-white">Toodler Group
-                      </h1>
-                      <p class="text-sm text-red-600">1-5 years old</p>
-                  </div>
-
-                  <div class="">
-                      <input type="number" id="first_name"
-                          class="my-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-32 h-8 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                  </div>
-              </div>
 
                 </div>
 
@@ -225,10 +199,25 @@
                         </div>
                     </div>
 
+                    <div class="flex justify-between">
+                    <div class="flex items-center pl-4 w-[40%] rounded dark:border-gray-700">
+                        <input id="bordered-radio-1" type="radio" value="" name="bordered-radio"
+                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                        <label for="bordered-radio-1"
+                            class="w-full py-2 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Payy Full</label>
+                    </div>
+                    <div class="flex items-center pl-4 w-[40%] rounded dark:border-gray-700">
+                        <input checked id="bordered-radio-2" type="radio" value="" name="bordered-radio"
+                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                        <label for="bordered-radio-2"
+                            class="w-full py-2 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">DP 50%</label>
+                    </div>
+                  </div>
+
 
                     <!-- Total -->
 
-                    <div class="mt-4 flex items-center justify-between">
+                    <div class="flex items-center justify-between">
 
                         <div class="mt-6 border-t border-b py-2 w-full">
                             <div class="flex items-center justify-between">
